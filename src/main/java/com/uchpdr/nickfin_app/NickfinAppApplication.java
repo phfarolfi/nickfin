b967ebb9583ac0378bd39b4a94f9a4847d034f64
pedro.farolfi <pedro.farolfi@intelie.com.br>
2024-12-16 18:44:41 -0300

    build: add default mvn files

---
+++ src/main/java/com/uchpdr/nickfin_app/NickfinAppApplication.java
@@ -0,0 +1,13 @@
+package com.uchpdr.nickfin_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NickfinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NickfinAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uchpdr/nickfin_app/NickfinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uchpdr.nickfin_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NickfinAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
