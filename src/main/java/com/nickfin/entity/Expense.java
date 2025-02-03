9cf158c4dd4e215ad317f5e39d23208a74736097
pedro.farolfi <pedro.farolfi@intelie.com.br>
2025-02-03 15:45:30 -0300

    feat: create entity to handle expenses and revenues

--- src/main/java/com/nickfin/entity/Expense.java
+++ src/main/java/com/nickfin/entity/Expense.java
@@ -3,45 +3,11 @@ package com.nickfin.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "expense")
-public class Expense {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private Long date;
-    private Long createdAt;
+@DiscriminatorValue("expense")
+public class Expense extends Entry {
     private Long installmentNumber;
-    private String description;
-    private String paymentMethod;
-    private String payer;
     private String payee;
-    private String category;
-    private Double amount;
     private Boolean paid;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getDate() {
-        return date;
-    }
-
-    public void setDate(Long date) {
-        this.date = date;
-    }
-
-    public Long getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Long createdAt) {
-        this.createdAt = createdAt;
-    }
 
     public Long getInstallmentNumber() {
         return installmentNumber;
@@ -51,52 +17,12 @@ public class Expense {
         this.installmentNumber = installmentNumber;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public String getPayer() {
-        return payer;
-    }
-
-    public void setPayer(String payer) {
-        this.payer = payer;
-    }
-
     public String getPayee() {
         return payee;
     }
 
     public void setPayee(String payee) {
         this.payee = payee;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
     }
 
     public Boolean getPaid() {
